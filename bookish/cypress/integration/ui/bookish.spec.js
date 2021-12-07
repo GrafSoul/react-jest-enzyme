3e1940dcad483155edcb421aa20eb86c4c5432a0
GrafSoul <grafsoul@gmail.com>
2021-12-07 15:24:59 +0500

    feat: Added Bookisj project

--- bookish/cypress/integration/ui/bookish.spec.js
+++ bookish/cypress/integration/ui/bookish.spec.js
@@ -1,7 +1,50 @@
-/* eslint-disable no-undef */
-describe("Bookish application", function () {
-  it("Visits the bookish", function () {
-    cy.visit("http://localhost:3000/");
-    cy.get('h2[data-test="heading"]').contains("Bookish");
+import {
+  checkAppTitle,
+  checkBookDetail,
+  checkBookListWith,
+  checkReview,
+  cleanUpStubBooks,
+  composeReview,
+  feedStubBooks,
+  gotoApp,
+  gotoNthBookInTheList,
+  performSearch
+} from "../../helpers";
+
+describe('Bookish application', () => {
+  beforeEach(() => {
+    feedStubBooks();
+    gotoApp();
   });
+
+  afterEach(() => {
+    cleanUpStubBooks();
+  });
+
+  it('Visits the bookish', () => {
+    checkAppTitle();
+  });
+
+  it('Shows a book list', () => {
+    checkBookListWith(['Refactoring', 'Domain-driven design', 'Building Micro-service']);
+  });
+
+  it('Goes to detail page', () => {
+    gotoNthBookInTheList(0);
+    checkBookDetail();
+  });
+
+  it('Search for a title', () => {
+    checkBookListWith(['Refactoring', 'Domain-driven design', 'Building Micro-service']);
+    performSearch('design');
+    checkBookListWith(['Domain-driven design']);
+  });
+
+  it('Write a review for a book', () => {
+    gotoNthBookInTheList(0);
+    checkBookDetail();
+    composeReview('Juntao Qiu', 'Excellent works!');
+    checkReview();
+  });
+
 });
--- bookish/src/App.js
+++ bookish/src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
-import { Typography } from "@material-ui/core";
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import {Route, Switch} from 'react-router-dom';
+import BookListContainer from "./BookList/BookListContainer";
+import BookDetailContainer from "./BookDetail/BookDetailsContainer";
+import Container from "@material-ui/core/Container/Container";
 
-function App() {
+const App = () => {
   return (
-    <div>
+    <Container maxWidth="sm">
       <Typography variant="h2" component="h2" data-test="heading">
         Bookish
       </Typography>
-    </div>
+      <Switch>
+        <Route exact path="/" component={BookListContainer}/>
+        <Route path="/books/:id" component={BookDetailContainer}/>
+      </Switch>
+    </Container>
   );
 }
 
